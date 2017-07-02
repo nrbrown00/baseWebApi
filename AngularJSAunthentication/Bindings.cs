eeae51208a00eab02c92e3e6b5f7700a7d5061bb
nrbrown00 <nrbrown@gmail.com>
2017-07-02 13:09:56 -0500

    initial commit

---
+++ AngularJSAunthentication/Bindings.cs
@@ -0,0 +1,17 @@
+﻿using AngularJSAunthentication.API.Interfaces;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularJSAunthentication.API
+{
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IAuthRepository>().To<AuthRepository>();
+        }
+    }
+}
---
+++ AngularJSAunthentication/Interfaces/IAuthRepository.cs
@@ -0,0 +1,17 @@
+﻿using AngularJSAunthentication.API.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AngularJSAunthentication.API.Interfaces
+{
+    public interface IAuthRepository
+    {
+        Task<IdentityResult> RegisterUser(UserModel userModel);
+        Task<IdentityUser> FindUser(string userName, string password);
+    }
+}
